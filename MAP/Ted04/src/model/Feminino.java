3d532594bf636f0f379a4fc430c82af5bfb7c00a
João Henrique Garcia <joaogarcia.sistemas@gmail.com>
2021-03-29 20:23:49 -0300

    create ted04

---
+++ MAP/Ted04/src/model/Feminino.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Feminino extends Saudacao{
+
+	public Feminino(String genero) {
+		super (genero);
+		
+	}
+	
+	public String Mensagem(){
+		return "Bem vinda Sra. ";
+	}
+	
+
+}
---
+++ MAP/Ted04/src/model/NaoInformado.java
@@ -0,0 +1,14 @@
+package model;
+
+public class NaoInformado extends Saudacao{
+
+	public NaoInformado(String genero) {
+		super (genero);
+				
+	}
+		
+	public String Mensagem(){
+		return "Bem vindo(a): ";
+	}
+
+}
