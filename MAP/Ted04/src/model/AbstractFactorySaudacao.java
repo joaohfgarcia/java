0a656bc8e65d609be8f493ec600ec5c2a49073a0
João Henrique Garcia <62037961+joaohfgarcia@users.noreply.github.com>
2021-07-20 21:46:53 -0300

    Delete MAP directory

--- MAP/Ted04/src/model/AbstractFactorySaudacao.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public abstract class AbstractFactorySaudacao {
-
-	public abstract Saudacao getSaudacao(String genero);
-
-}
--- MAP/TedAdapter/src/model/SomadorEsperado.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public interface SomadorEsperado {
-	
-	int somaVetor(int[] vetor);
-
-}
